e2d8dd4e31321ae24c48c0e4e19763030e533ce7
Christine Wulffeld <christinedk11@hotmail.com>
2019-04-30 10:54:25 +0200

    .

---
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -0,0 +1,6 @@
+package com.example.demo.service;
+
+public interface AdminService {
+
+    public void tjekPassword(String username, String password);
+}
---
+++ src/main/java/com/example/demo/service/AdminServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+public class AdminServiceImpl implements AdminService{
+
+
+    @Override
+    public void tjekPassword(String username, String password) {
+
+    }
+}
