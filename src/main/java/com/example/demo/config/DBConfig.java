2926add98e373c3ad90f6c9a775788709153d114
SoMohr <sofi591r@stud.kea.dk>
2019-04-30 14:31:51 +0200

    hej

--- src/main/java/com/example/demo/config/DBConfig.java
+++ src/main/java/com/example/demo/config/DBConfig.java
@@ -17,7 +17,6 @@ public class DBConfig {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-
         System.out.println("3");
         Connection connect = DriverManager
                 .getConnection("jdbc:mysql://den1.mysql6.gear.host:3306/datamatiker18a", "datamatiker18a", "Ie5qK~3?U8Zy");
--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.AdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
@@ -13,6 +16,9 @@ import javax.servlet.http.HttpSession;
 public class MenuController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    AdminService adminService;
 
     @GetMapping("/")
     public String index() {
@@ -31,14 +37,16 @@ public class MenuController {
     }
 
     @PostMapping("/login")
-    public String login(HttpSession session, WebRequest wr){
+    public String login(HttpSession session, WebRequest wr, Model model){
 
         String user = wr.getParameter("user");
         String password = wr.getParameter("password");
-        if(user.equals("admin") && password.equals("test")) {
+        if(adminService.tjekPassword(user, password)) {
             session.setAttribute("logged_in", true);
             return "adminPage";
         }
+
+        model.addAttribute("error", true);
 
         return "login";
     }
