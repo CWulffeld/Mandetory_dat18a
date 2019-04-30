c2fc6eec7a2cf002617c9c73e731f2bf3d7f6e03
SoMohr <sofi591r@stud.kea.dk>
2019-04-30 10:59:07 +0200

    hej

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MenuController {
@@ -26,15 +30,28 @@ public class MenuController {
         return "login";
     }
 
+    @PostMapping("/login")
+    public String login(HttpSession session, WebRequest wr){
 
-    @GetMapping("/adminPage")
-    public String adminPage(){
-        log.info("adminPage action called...");
+        String user = wr.getParameter("user");
+        String password = wr.getParameter("password");
+        if(user.equals("admin") && password.equals("test")) {
+            session.setAttribute("logged_in", true);
+            return "adminPage";
+        }
 
-        log.info("adminPage action ended...");
-
-        return "adminPage";
+        return "login";
     }
+
+
+//    @GetMapping("/adminPage")
+//    public String adminPage(){
+//        log.info("adminPage action called...");
+//
+//        log.info("adminPage action ended...");
+//
+//        return "adminPage";
+//    }
 
 
     @GetMapping("/index")
